bdad5c8de07cbd2a8bf713fa645a76d4d1355e42
Alex Thomassen <alex@thomassen.xyz>
2019-10-21 12:28:05 +0200

    Initial commit - Complete, but untested bot

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+const config = {
+    Discord: {
+        /**
+         * Bot token for logging into Discord.
+         */
+        token: '',
+    }
+};
+
+module.exports = config;
---
+++ index.js
@@ -0,0 +1,78 @@
+const DiscordJs = require('discord.js');
+const Permissions = DiscordJs.Permissions.FLAGS;
+const config = require('./config.js');
+
+const client = new DiscordJs.Client();
+const codeTicks = '```';
+
+/**
+ * Handle region change
+ */
+client.on('message', async (message) => {
+    const member = message.member;
+    const user = message.author;
+
+    if (!member) {
+        return;
+    }
+
+    const canMoveRegion = member.hasPermission(Permissions.MANAGE_GUILD, false, true, true);
+    if (!canMoveRegion) {
+        return;
+    }
+
+    const content = message.cleanContent;
+    const params = content.split(' ');
+    const cmd = params[0].toLowerCase();
+
+    if (cmd !== '!v') {
+        return;
+    }
+
+    const guild = message.guild;
+    try {
+        let regions = await guild.fetchVoiceRegions();
+    }
+    catch (err) {
+        await message.reply(`${message.author} - Unable to get voice regions.`);
+        console.error(err);
+        return;
+    }
+
+    /**
+     * Find regions that are still available to switch to.
+     */
+    regions = regions.filter((region) => {
+        return region.deprecated;
+    });
+
+    const formatRegions = regions.map((region) => {
+        return `${region.name} [${region.id}]`;
+    });
+
+    const selectedRegion = params[1];
+    const listRegions = `**Available regions - 'Region name [Region ID]':**\n${codeTicks}- ${formatRegions.join('\n- ')}${codeTicks}\n\n**Usage:** \`!v region-id\``;
+
+    if (!selectedRegion) {
+        await message.reply(listRegions);
+        return;
+    }
+
+    const getRegion = regions.get(selectedRegion);
+    if (!getRegion) {
+        await message.reply('**Invalid region specified**\n' + listRegions);
+        return;
+    }
+
+    try {
+        await guild.setRegion(getRegion, `Voice region updated to ${selectedRegion} by ${user.username}#${user.discriminator}.`);
+        await message.reply(`${member} - Voice region updated to: ${getRegion}`);
+    }
+    catch (err)
+    {
+        await message.reply(`${member} - Error updating region.`);
+        console.error(err);
+    }
+});
+
+client.login(config.Discord.token);
